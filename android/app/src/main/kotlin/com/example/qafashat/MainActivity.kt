81e38ce5e8253c7d191c688cffeffeb44f10e1f2
MohamedDeghedy <mohamed.n.deghedy@gmail.com>
2021-08-05 14:26:36 +0200

    My First Commit - Splash Screen

---
+++ android/app/src/main/kotlin/com/example/qafashat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qafashat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+
+Future<String> loadAsset() async {
+  return await rootBundle.loadString('assets/config.json');
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+
+    
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[700],
+      body: Center(
+        child: Image.asset('assets/images/logo.png', width: MediaQuery.of(context).size.width * 0.65
+        ),
+      ),
+    );
+  }
+}
